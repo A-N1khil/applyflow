5900acade39c030cd45450d43737981e5258d6eb
A-N1khil <nikhilanand1006@gmail.com>
2026-08-27 16:05:11 -0400

    Login component

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -4,6 +4,10 @@ import { LoginForm } from "@/components/login-form"
 import { GalleryVerticalEndIcon } from "lucide-react"
 
 export default function LoginPage() {
+  function handleLogin(email: string) {
+    console.log(email)
+  }
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
@@ -13,7 +17,7 @@ export default function LoginPage() {
           </div>
           Acme Inc.
         </a>
-        <LoginForm />
+        <LoginForm onLogin={handleLogin} />
       </div>
     </div>
   )
