6d09e32c5407438eb52897079f29365cf67a7878
A-N1khil <nikhilanand1006@gmail.com>
2026-08-31 22:34:15 -0400

    two signups

--- client/app/signup/page.tsx
+++ client/app/signup/page.tsx
@@ -1,17 +1,28 @@
 "use client"
 
-import { SignupForm } from "@/components/signup-form"
+import { useState } from "react"
+import { SignupForm } from "@/components/signup-form-entry"
+import { SignupDetailedForm } from "@/components/signup-form-detailed"
 
 export default function SignupPage() {
+  const [step, setStep] = useState(1)
+
   function handleSignup(email: string) {
     console.log(email)
+    setStep(2)
   }
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <SignupForm onSignup={handleSignup} />
-      </div>
+      {step === 1 ? (
+        <div className="w-full max-w-sm">
+          <SignupForm onSignup={handleSignup} />
+        </div>
+      ) : (
+        <div className="w-full max-w-sm">
+          <SignupDetailedForm />
+        </div>
+      )}
     </div>
   )
 }
